3c1b390375fd99da7129d03e6376685c66608c96
nolindayao1 <nolindayao.student@asiancollege.edu.ph>
2025-04-11 12:24:32 +0800

    add file

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def contact():
+    name =request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    message = request.form.get('message')
+    subject = request.form.get('subject')
+    preferred  = request.form.get('preferred')
+    agreement = request.form.get('agreement')
+    return render_template('contact_form.html', name=name, email=email, phone=phone, message=message, subject=subject, preferred=preferred, agreement=agreement)
+
+
+
+@app.route('/confirmation', methods = ['POST', 'GET'])
+def confirm():
+    return render_template('confirmation.html')
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
