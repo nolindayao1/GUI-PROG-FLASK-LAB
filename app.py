4e4ce8b137c723ce5cd14d82b6b8936661ffd840
nolindayao1 <nolindayao.student@asiancollege.edu.ph>
2025-04-12 16:20:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def contact():
-    name =request.form.get('name')
+    return render_template('contact_form.html')
+
+@app.route('/confirmation', methods=['POST'])
+def confirm():
+    name = request.form.get('name')
     email = request.form.get('email')
     phone = request.form.get('phone')
     message = request.form.get('message')
     subject = request.form.get('subject')
-    preferred  = request.form.get('preferred')
+    preferred = request.form.get('preferred')
     agreement = request.form.get('agreement')
-    return render_template('contact_form.html', name=name, email=email, phone=phone, message=message, subject=subject, preferred=preferred, agreement=agreement)
 
-
-
-@app.route('/confirmation', methods = ['POST', 'GET'])
-def confirm():
-    return render_template('confirmation.html')
-
-
+    return render_template('confirmation.html', name=name, email=email, phone=phone,
+                           message=message, subject=subject, preferred=preferred, agreement=agreement)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
